cd4544531e687f39f3bc684884c9a5b735583fd2
HRNPH <51855316+HRNPH@users.noreply.github.com>
2022-12-14 00:06:59 +0700

    add image display & generate button

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -18,23 +19,38 @@ class _MyAppState extends State<MyApp> {
       title: 'Ganime_Fullbody',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("GANime App"),
+          backgroundColor: Colors.cyanAccent,
+          title: const FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(
+              "Generate Anime Girl Using Gan",
+              style: TextStyle(
+                fontFamily: 'Kanit',
+                fontSize: 30,
+              ),
+            ),
+          ),
         ),
         body: Container(
-          margin: const EdgeInsets.only(top: 10),
+          color: Colors.white,
           alignment: Alignment.center,
           child: Column(
             children: <Widget>[
-              const Text(
-                "Generate Anime Girl Using Gan",
-                style: TextStyle(
-                  fontFamily: 'Kanit',
-                  fontSize: 30,
-                ),
-              ),
               Wrap(
-                spacing: 2.0,
-                children: <Widget>[Image.asset('assets/images/ruok.png')],
+                alignment: WrapAlignment.center,
+                direction: Axis.horizontal,
+                spacing: 300.0,
+                children: <Widget>[
+                  const CharacterDisplay(),
+                  Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    width: 200,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Generate Gans Girl!"),
+                    ),
+                  )
+                ],
               ),
             ],
           ),
@@ -42,4 +58,38 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
+}
+
+class CharacterDisplay extends StatefulWidget {
+  const CharacterDisplay({super.key});
+
+  @override
+  State<CharacterDisplay> createState() => _CharacterDisplayState();
+}
+
+class _CharacterDisplayState extends State<CharacterDisplay> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      margin: const EdgeInsets.only(top: 20),
+      width: MediaQuery.of(context).size.width * 0.5,
+      height: MediaQuery.of(context).size.height * 0.2,
+      child: Image.asset('assets/images/placeholder.png'),
+      // background colour
+    );
+  }
+}
+
+Future<http.Response> fetchimage() {
+  return http.get(
+    Uri.parse(
+      'https://http://ec2-18-141-159-239.ap-southeast-1.compute.amazonaws.com/api',
+    ),
+  );
 }
