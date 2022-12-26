37804f368fd6c4bd81f0793c8a424c24e91483b5
HRNPH <51855316+HRNPH@users.noreply.github.com>
2022-12-26 20:42:37 +0700

    del Debug banner

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -21,6 +21,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Ganime_Fullbody',
       home: Scaffold(
         appBar: AppBar(
